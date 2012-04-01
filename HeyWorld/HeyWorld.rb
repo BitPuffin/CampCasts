3a3f0d21e6ec99414ed3b4a942cfd33c2faa488d
Isak Andersson <MilkshakePanda@live.se>
2012-04-01 20:07:51 +0200

    Made the hello world a bit more mvc and added a secret page to demonstrate how layout and yield works

--- HeyWorld/HeyWorld.rb
+++ HeyWorld/HeyWorld.rb
@@ -3,7 +3,34 @@ Camping.goes :HeyWorld
 module HeyWorld::Controllers
 	class Index < R '/'
 		def get
-			"Hey World!"
+			render :root
 		end
+	end
+	
+	class SecretMessage < R '/secret'
+		def get
+			render :message
+		end
+	end
+end
+
+module HeyWorld::Views
+	def layout
+		doctype!
+		head do
+			title "yo lads!"
+		end
+		body do
+			h1 "Cool site"
+			self << yield
+		end
+	end
+	
+	def root
+		p "Hey World!"
+	end
+	
+	def message
+		p "Camping is amazing"
 	end
 end
