59255f130b449baa14eebd236ff689e5a637b485
Isak Andersson <MilkshakePanda@live.se>
2012-04-03 10:19:50 +0200

    Started working a little bit on a blog example

---
+++ Blog/blog.rb
@@ -0,0 +1,45 @@
+Camping.goes :Blog
+
+# Anything but a slash ([^/]+)
+# Number (\d+)
+
+module Blog::Controllers
+	class Index < R '/'
+		def get
+		
+		end
+	end
+	
+	class Show < R '/(\d+)'
+		def get(id)
+		
+		end
+	end
+end
+
+module Blog::Views
+	
+end
+
+module Blog::Models
+	class Post < Base
+	end
+	
+	class PostFields < V 1.0
+		def self.up
+			create_table Page.table_name do |t|
+				t.string 	:title
+				t.text		:content
+				t.timestamps
+			end
+		end
+		
+		def self.down
+			drop_table Page.table_name
+		end
+	end
+end
+
+def Blog.create
+	Blog::Models.create_schema
+end
