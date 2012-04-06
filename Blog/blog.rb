744ffae0bb18412b352521567796895483bec997
Isak Andersson <BitPuffin@lavabit.com>
2012-04-06 12:23:24 +0200

    Worked on the Blog application for screecast

--- Blog/blog.rb
+++ Blog/blog.rb
@@ -6,23 +6,94 @@ Camping.goes :Blog
 module Blog::Controllers
 	class Index < R '/'
 		def get
+				@posts = Post.all :select => "title"
+				render :index
+		end
+	end
+	
+	class Create < R '/new'
+		def get
+			@post = Post.new
+			render :new
+		end
 		
+		def post
+			@post.save
 		end
 	end
 	
 	class Show < R '/(\d+)'
 		def get(id)
-		
+			@post = Post.find_by_id(id)
+			render :show
 		end
 	end
 end
 
 module Blog::Views
+	def layout
+		doctype!
+		head do
+			title 'Blog'
+		end
+		body do
+			h1 'My Blog'
+			self << yield
+		end
+	end
 	
+	def index
+		ul do
+			@posts.each do |post|
+				li do
+					a post.title, :href => R(Show, post.id)
+				end
+			end
+		end
+	end
+	
+	def show
+		h2 @post.title
+		self << post.content
+	end
+	
+	def new
+		h2 "New Blog Post!"
+		form :action => R(Create), :method => post do
+				textarea @post.title, :name => :title,
+					:rows =>1, :cols => 50
+					
+				br
+				
+				textarea @post.content, :name => :content,
+				:rows => 10, :cols => 50
+				
+				br
+				
+				input :type => :submit, :value => "Submit!"
+		end
+	end
+	
+	def edit
+		h2 "Edit Blog Post!"
+		form :action => R(Show, @post.id), :method => post do
+			textarea @post.title, :name => :title,
+					:rows =>1, :cols => 50
+			
+			br
+				
+			textarea @post.content, :name => :content,
+				:rows => 10, :cols => 50
+				
+			br
+			
+			input :type => :submit, :value => "Submit!"
+		end
+	end
 end
 
 module Blog::Models
-	class Post < Base
+	class Post < Base		#ERR: ActiveRecord tells me that the Post table doesn't exist
 	end
 	
 	class PostFields < V 1.0
